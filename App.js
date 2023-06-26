a682fb08a87f284232e0b8f987375ceab239a98a
Lyonsmasawa <lyonsmasawa@gmail.com>
2023-06-26 07:25:12 -0700

    walkthrugh: 'get familiar'

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/favicon.png'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} resizeMode='contain' style={styles.logo} />
+      <Button title='+' onPress={() => {
+       return alert("we just made our first app")
+      }} />
     </View>
   );
 }
@@ -14,7 +16,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+
+  logo: {
+    width: 150,
+    height: 50,
+    padding: 5,
+    
+  }
 });
