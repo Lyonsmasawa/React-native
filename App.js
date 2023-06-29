ca3bb443ef21a3a3726319d61aaa728c7628cb4e
Lyonsmasawa <lyonsmasawa@gmail.com>
2023-06-29 06:26:51 -0700

    build: 'tab navigator'

--- App.js
+++ App.js
@@ -4,11 +4,16 @@ import * as React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./app/screens/Home";
 import Profile from "./app/screens/Profile";
 import Header from "./app/components/Header";
 import Constants from "expo-constants";
+import { AntDesign } from "@expo/vector-icons";
+import Search from "./app/screens/Search";
+import Notifications from "./app/screens/Notifications";
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const CUSTOM_THEME = {
@@ -24,12 +29,52 @@ function App() {
     <View style={styles.container}>
       <Header />
       <NavigationContainer theme={CUSTOM_THEME}>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Profile" component={Profile} />
-        </Stack.Navigator>
+        <MyTabs />
       </NavigationContainer>
     </View>
+  );
+}
+const HomeNavigators = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Profile" component={Profile} />
+    </Stack.Navigator>
+  );
+};
+
+function MyTabs() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={size} color={color} />
+          ),
+          title: 'Home',
+        }}
+        name="HomeScreen"
+        component={HomeNavigators}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="search1" size={size} color={color} />
+          ),
+        }}
+        name="Search"
+        component={Search}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="hearto" size={size} color={color} />
+          ),
+        }}
+        name="Notifications"
+        component={Notifications}
+      />
+    </Tab.Navigator>
   );
 }
 
---
+++ app/screens/Notifications.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Notifications = () => {
+    return (
+        <View>
+            <Text>Testtstts</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Notifications;
